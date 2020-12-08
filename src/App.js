ca9eaf99935ed72822dd3aa52c497574d39859b8
Sam Sheets <sam.sheets94@gmail.com>
2020-12-07 17:46:47 -0700

    search component interactivity

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Search from './Search/Search';
-import Results from './Results/Results';
+
 
 // const results = [
 //   {
@@ -24,41 +24,12 @@ import Results from './Results/Results';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      results: []
-    }
-  }
-
-  componentDidMount() {
-    
-    const url = `https://www.googleapis.com/books/v1/volumes?q=surf&download=epub&printType=books&key=AIzaSyCT2hnLLjqh_M0NOWqffJYY_or9dzoMDvI`;
-    fetch(url)
-        .then(response => {
-            if(response.ok) {
-                return response.json();
-            } else {
-                throw new Error ('Something happened, please try again later.')
-            }
-        })
-        .then(data => {
-            console.log(data)
-            
-            this.setState({results: data})
-        })
-        .catch(error => {
-            console.log(error);
-            this.setState({error: error.message});
-        });
-  }
-
   render() {
     return (
       <div className="App">
         <h1>Google Book Search</h1>
         <Search />
-        <Results results={this.state.results}/>
+        
       </div>
     );
   }
--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,73 +1,80 @@
 import React, {Component} from 'react';
+import Results from '../Results/Results';
 
 
 
 
 class Search extends Component {
 
-
     constructor(props) {
         super(props);
-        this.setState = {
-        term: "test",
-        bookType: " ",
-        printType: " "
-        };
-    }
+        this.state = {
+          results: [],
+          termValue: " ",
+          printTypeValue: " "
+        }
+        
+      }
 
   
 
-  termChanged(term) {
-      this.setState({term})
-  }
+    termChanged(termValue) {
+        this.setState({termValue})
+    }
 
-  printTypeChanged(printType) {
-      this.setState({printType})
-  }
-
-  bookTypeChanged(bookType) {
-      this.setState({bookType})
-  }
-
-  handleSubmit(e) {
-      console.log('I was clicked!');
-      e.preventDefault();
-  }
+    printTypeChanged(printTypeValue) {
+        this.setState({printTypeValue})
+    }
 
 
-  render() {
-      
-    return (
-      <div className='Search'>
-          <form className='searchForm' onSubmit={e => this.handleSubmit(e)}>
-              <label htmlFor='search'>Search: </label>
-              <input 
-              type='text' 
-              name='search'
-              
-              onChange={e => this.termChanged(e.target.value)}/>
-              <label htmlFor='printType'>Print Type: </label>
-              <select 
-              name='printType' 
-              
-              onChange={e => this.printTypeChanged(e.target.value)}>
-                <option value='all'>All</option>
-                <option value='books'>Books</option>
-                <option value='magazine'>Magazine</option>
-              </select>
-              <label htmlFor='bookType'>Book Type: </label>
-              <select 
-              name='bookType' 
-              
-              onChange={e => this.bookTypeChanged(e.target.value)}>
-                <option value='epub'>epub</option>
-                <option value=''>all</option>
-                
-              </select>
-              <button type='submit'>Search</button>
-          </form>
-          
-      </div>
+    handleSubmit(e) {
+        console.log('I was clicked!');
+        e.preventDefault();
+        const url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.termValue}&printType=${this.state.printTypeValue}&key=AIzaSyCT2hnLLjqh_M0NOWqffJYY_or9dzoMDvI`;
+        fetch(url)
+            .then(response => {
+                if(response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error ('Something happened, please try again later.')
+                }
+            })
+            .then(data => {
+                console.log(data)
+                this.setState({results: data})
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({error: error.message});
+            });
+    }
+
+
+    render() {
+        
+        return (
+        <div className='Search'>
+            <form className='searchForm' onSubmit={e => this.handleSubmit(e)}>
+                <label htmlFor='search'>Search: </label>
+                <input 
+                type='text' 
+                name='search'
+                value={this.state.term}
+                onChange={e => this.termChanged(e.target.value)}/>
+                <label htmlFor='printType'>Print Type: </label>
+                <select 
+                name='printType' 
+                value={this.state.printType}
+                onChange={e => this.printTypeChanged(e.target.value)}>
+                    <option value='all'>All</option>
+                    <option value='books'>Books</option>
+                    <option value='magazine'>Magazine</option>
+                </select>
+
+                <button type='submit'>Search</button>
+            </form>
+            <Results results={this.state.results}/>
+        </div>
     );
   }
 
